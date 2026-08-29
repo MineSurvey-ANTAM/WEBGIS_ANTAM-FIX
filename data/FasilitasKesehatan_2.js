e4fe1ce4f04b12e5ea3be9f5ae26ddbd2c1d86e3
shellie amelia <luthfiramdani2004@gmail.com>
2026-08-29 16:56:47 +0700

    Add files via upload

---
+++ data/FasilitasKesehatan_2.js
@@ -0,0 +1,1 @@
+var json_FasilitasKesehatan_2 = {"type":"FeatureCollection","name":"FasilitasKesehatan_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Faskes":"Puskesmas Antam"},"geometry":{"type":"Point","coordinates":[106.570820278821458,-6.657980788134841]}}]}
